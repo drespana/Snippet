824bdcac5bdcb93bbb3eba40aa11459166ae16fe
drespana <drespana@google.com>
2024-01-05 16:26:22 -0600

    mvp

--- server/routes/snippets.js
+++ server/routes/snippets.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {snippets} = require("../db/seed.js")
+const {snippets} = require("../db/sample.js")
 
 const snippetRouter = express.Router();
 snippetRouter.use(express.json());
@@ -15,10 +15,30 @@ snippetRouter.get("/", (req, res) => {
 })
 
 // GET snippet by ID
+snippetRouter.get("/:id", (req, res, next) => {
+    try {
+        const search = req.params.id-1
+        const found = snippets[search]
+        res.status(200).send(found)
+    } catch (err) {
+        next(err)
+    }
+})
 
 // GET snippet by LANGUAGE
 
 // POST snippet
+snippetRouter.post("/", (req, res, next) => {
+    try{
+        let mkSnip = req.body;
+        snippets.push(mkSnip);
+        const created = snippets.length;
+        res.status(200).send(snippets[created])
+    } catch (err) {
+        next(err)
+    }
+})
+
 
 
 module.exports = snippetRouter;
