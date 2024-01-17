cd91f44d7df34edca6cc014c7bae003a279e424f
drespana <drespana@google.com>
2024-01-17 09:59:31 -0600

    merged run & app

--- server/app.js
+++ server/app.js
@@ -1,31 +1,45 @@
 require('dotenv').config();
 const express = require('express')
 const snippetRouter = require('../server/src/routes/snippet');
-// const { auth } = require('express-openid-connect');
-
-
-// const config = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   secret: 'a long, randomly-generated string stored in env',
-//   baseURL: 'http://localhost:4000',
-//   clientID: 'r6k6Qugzo6DmFAuSjjmwtkiE9WlexKzr',
-//   issuerBaseURL: 'https://dev-kqcvt5qlx045drmf.us.auth0.com'
-// };
-// app.use(auth(config))
-//
-// app.get("/", (req, res) => {
-//     res.send("Snipper Snippets API");
-//   });
-
-
+const { auth } = require('express-openid-connect');
+const {db} = require('./src/db/config')
+const PORT = process.env.PORT || 3000;
 const app = express()
+
+///////////// Really don't want this here ////////////////////////////
+const {Snippet} = require('./src/models/Snippet')
+const {snippets} = require('./src/db/sample')
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:4000',
+  clientID: 'r6k6Qugzo6DmFAuSjjmwtkiE9WlexKzr',
+  issuerBaseURL: 'https://dev-kqcvt5qlx045drmf.us.auth0.com'
+};
+app.use(auth(config))
+
+app.get("/", (req, res) => {
+    res.send("Snipper Snippets API");
+  });
+
 
 app.use(express.json())
 
 // app.use('/user', routes.user)
 app.use('/snippet', snippetRouter)
 
-module.exports = {
-    app
+
+async function init() {
+    db.sync()
+    // snippets.map((snip)=>Snippet.create(snip))
+    // const all = await Snippet.findAll()
+    // console.log(all)
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+      }) 
+    
 }
+
+init();
--- server/src/db/seed.js
+++ server/src/db/seed.js
@@ -3,7 +3,7 @@ const { Snippet } = require('../models/Snippet')
 const { db } = require('./config')
 
 const syncSeed = async ()=> {
-    await db.sync({force:true});
+    await db.sync({force:true})
     snippets.map(snip => Snippet.create(snip))
 }
 
