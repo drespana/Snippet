6741584513b4b9f85b79cca876dcfe188daea6b8
drespana <drespana@google.com>
2024-01-22 14:28:14 -0600

    clean up

--- server/src/routes/snippet.js
+++ server/src/routes/snippet.js
@@ -2,7 +2,7 @@ const express = require('express');
 const snippetRouter = express.Router();
 const { Snippet } = require('../models/Snippet');
 const authorize = require('../middleware/authorize');
-const { encrypt, decrypt} = require('../utils/encrypt');
+const { encrypt, decrypt } = require('../utils/encrypt');
 
 const encryptedSnippets = require('../db/test');
 
@@ -16,39 +16,6 @@ snippetRouter.get('/', async (req, res, next)  => {
     }
 })
 
-/////////////////////////////////////////////////////
-////////////// testing encrypt file ////////////////
-///////////////////////////////////////////////////
-
-// let id = encryptedSnippets.length;
-// snippetRouter.get('/test', (req, res, next) => {
-//     try{
-//         const decryptedSnippets = decrypt(encryptedSnippets)
-//         res.json(decryptedSnippets)
-//     } catch (error) {
-//         next(error)
-//     }
-// })
-// snippetRouter.post('/test', (req, res, next) => {
-//     const { language, code } = req.body
-//     try {
-//         const snippet = {
-//             id: id++,
-//             language,
-//             code
-//         }
-//         encryptedSnippets.push({...encryptedSnippets, code: encrypt(code)});
-//         res.status(201).json(snippet);
-//     } catch (error) {
-//         next(error)
-//     }
-// })
-
-
-///////////////////////////////////////////////////////
-///////////////// with encrypt file //////////////////
-//////////////////////////////////////////////////////
-
 // get by ID
 
 
@@ -56,22 +23,22 @@ snippetRouter.get('/', async (req, res, next)  => {
 
 
 // create a new snippet
-// snippetRouter.post('/', async (req, res) => {
-//     const { language, code } = req.body
+snippetRouter.post('/', async (req, res) => {
+    const { language, code } = req.body
 
-//     if (!language || !code) {
-//         return res
-//             .status(400)
-//             .json({error: 'language and code are required fields'})
-//     }
+    if (!language || !code) {
+        return res
+            .status(400)
+            .json({error: 'language and code are required fields'})
+    }
 
-//     const snippet = {
-//         id: ++id,
-//         language,
-//         code
-//     }
+    const snippet = {
+        id: ++id,
+        language,
+        code
+    }
 
-//     await Snippet.create({snippet, code: encrypt(code)});
-// })
+    await Snippet.create({snippet, code: encrypt(code)});
+})
 
 module.exports = snippetRouter;
